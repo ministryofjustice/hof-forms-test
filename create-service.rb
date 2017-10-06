cf94b37ab24114de5e52f80c01dfa270683d8f62
David Salgado <david.salgado@digital.justice.gov.uk>
2017-10-06 12:46:24 +0100

    WiP: Generate steps and fields

--- create-service.rb
+++ create-service.rb
@@ -11,9 +11,12 @@ end
 
 def add_hof_app(service)
   svc = service.downcase
+  fields = %w(name colour age)
+
   system "hof app #{svc}"
   set_title(svc, "#{service} Service")
-  write_index_js(svc)
+  write_fields_index_js(svc, fields)
+  write_index_js(svc, fields)
 end
 
 def set_title(service, title)
@@ -27,7 +30,21 @@ def set_title(service, title)
   )
 end
 
-def write_index_js(service)
+def write_fields_index_js(service, arr)
+  file = "apps/#{service}/fields/index.js"
+  fields = arr.map {|f| "  #{f}: { validate: 'required' }"}.join(",\n")
+
+  File.write(file, <<~EOF
+      'use strict';
+
+      module.exports = {
+      #{fields}
+      };
+    EOF
+  )
+end
+
+def write_index_js(service, fields)
   template = <<EOF
 'use strict';
 
@@ -35,10 +52,26 @@ module.exports = {
   name: '<%= service %>',
   baseUrl: '/<%= service %>',
   steps: {
-    '/name': {
-      fields: ['name'],
-      next: '/confirm'
-    },
+    #{steps(fields).join(',')},
+    <%= service_end %>
+  }
+};
+EOF
+
+  service_end = confirm_and_complete()
+
+  renderer = ERB.new(template)
+  File.write("apps/#{service}/index.js", renderer.result(binding))
+end
+
+def steps(fields)
+  [
+    step(name: 'name', fields: fields, next_step: 'confirm')
+  ]
+end
+
+def confirm_and_complete
+  <<EOF
     '/confirm': {
       behaviours: ['complete', require('hof-behaviour-summary-page')],
       next: '/complete'
@@ -46,13 +79,20 @@ module.exports = {
     '/complete': {
       template: 'confirmation'
     }
-  }
-};
 EOF
+end
 
-  renderer = ERB.new(template)
+def step(params)
+  name      = params.fetch(:name)
+  fields    = js_string_array(params.fetch(:fields))
+  next_step = params.fetch(:next_step)
 
-  File.write("apps/#{service}/index.js", renderer.result(binding))
+  <<EOF
+    '/#{name}': {
+      fields: #{fields},
+      next: '/#{next_step}'
+    }
+EOF
 end
 
 
@@ -67,5 +107,7 @@ __END__
 
 # TODO
 
+* Write translations
+* Handle pages
 * In add_hof_app, use service.snake_case, in case it has spaces
 
